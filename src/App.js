1ea30d258a7f9d56f330e6582a76ebd5bd2b22d6
JiahaoLi <andyofl1024@gmail.com>
2020-09-07 17:12:39 -0600

    adding auth context for app-wide state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 //import './App.css';
 
 // Export and define path components
@@ -10,37 +10,71 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from '../src/places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 // read all router dom url contents set up: npm install --save react-router-dom
 
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      // Switch component is to handle redirect action. If '/' has following path, it will not redirect back. Otherwise.
+      <Switch>
+        {/* The Route path will depend on '/'. Here we use 'exact' to only slash will work */}
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId" exact>
+          <UpdatePlace />
+        </Route>
+        {/* If the route path is not '/', then redirect to '/' */}
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth" exact>
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    )
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        {/* Switch component is to handle redirect action. If '/' has following path, it will not redirect back. Otherwise. */}
-        <Switch>
-          {/* The Route path will depend on '/'. Here we use 'exact' to only slash will work */}
-          <Route path="/" exact={true}>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId" exact>
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          {/* If the route path is not '/', then redirect to '/' */}
-          <Redirect to="/"></Redirect>
-        </Switch>
-      </main>
-    </Router>
+    // blind onchange new value and all routes listen to changes before redirect
+    // 
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
@@ -9,10 +9,13 @@ import {
   VALIDATOR_MINLENGTH
 } from '../../shared/util/Validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 // hooks
 const Auth = () => {
+  const auth = useContext(AuthContext);
+
   const [isLoignMode, setIsLoginMode] = useState(true);
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -59,6 +62,7 @@ const Auth = () => {
   const authSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
+    auth.login();
   };
 
   return (
